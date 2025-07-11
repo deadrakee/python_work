7623834a5aeab84f2e51000a1540be114b6bd414
EMEA\SAB1SF <boyan.sabchev@bg.bosch.com>
2025-07-11 18:07:34 +0300

    Creating the bullet class

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -12,7 +12,13 @@ class Settings:
         self.fullscreen = False
 
         # Ship settings
-        self.ship_speed = 2.5
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
 
     def change_bg_color_randomly(self):
         """Changes the background with a random color"""
